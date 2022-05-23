1a716e61155411316fd78430eaa86a8a2869eb84
youjeongPark <pyj0695@gmail.com>
2022-05-23 23:05:45 +0900

    add class

---
+++ NETThread/Counter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CounterClass
+{
+    class Counter
+    {
+        const int LOOP_COUNT = 100;
+
+        readonly object thisLock;
+
+        private int count;
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+
+        public Counter()
+        {
+            thisLock = new object();
+            count = 0;
+        }
+
+
+        public void Increase()
+        {
+            int loopCount = LOOP_COUNT;
+            while (loopCount-- > 0)
+            {
+                lock (thisLock)
+                {
+                    count++;
+                    Console.WriteLine("Increase" + count);
+                }
+                Thread.Sleep(1);
+            }
+        }
+
+
+        public void Decrease()
+        {
+            int loopCount = LOOP_COUNT;
+            while (loopCount-- > 0)
+            {
+                lock (thisLock)
+                {
+                    count--;
+                    Console.WriteLine("Decrease" + count);
+                }
+                Thread.Sleep(1);
+            }
+
+        }
+
+
+    }
+}
