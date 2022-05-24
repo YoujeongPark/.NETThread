9ae20d0e9fe0582a3e1297ace3ee5b1479781c42
youjeongPark <pyj0695@gmail.com>
2022-05-25 08:29:56 +0900

    Thread 잔여

---
+++ NETThread/MutexCounter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NETThread
+{
+    class MutexCounter
+    {
+        private static Mutex mutex = new Mutex();
+        private static int number = 100; 
+        public void Print(string _thread)
+        {
+
+            mutex.WaitOne();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(_thread + " : " + number--);
+            }
+            mutex.ReleaseMutex();
+        }
+
+        public void FirstWork()
+        {
+            Print("F");
+        }
+
+        public void SecondWork()
+        {
+            Print("S");
+        }
+    }
+}
--- NETThread/Program.cs
+++ NETThread/Program.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+using CounterClass;
+using NETThread;
 
 internal class Program
 {
+    static bool myFlag = false;
+
+    private static void PrintThreadState(ThreadState state)
+    {
+        Console.WriteLine("{0-16} : {1} ", state, (int)state);
+    }
+
 
     private static void ThreadFunc()
     {
@@ -18,7 +26,7 @@ internal class Program
 
     }
 
-    static bool myFlag = false;
+    
 
     private static void ThreadPolling()
     {
@@ -33,16 +41,21 @@ internal class Program
         }
     }
 
+
+
+
     static void Main(string[] args)
     {
         bool runServer = true;
         while (runServer)
         {
             Console.WriteLine("---------------------------");
-            Console.WriteLine("해당 작업을 선택하시오" +
+            Console.WriteLine("해당 작업을 선택하시오\n" +
             "1. Thread 실행  \n" +
-            "2. Thread 실행 Polling \n" +
-            "3. Thread 실행 Lock \n");
+            "2. Thread + Lambda \n" +
+            "3. Thread 실행 Polling + Abort\n" +
+            "4. Thread 실행 Polling  + Interrupt\n" +
+            "5. Thread 실행 Lock \n");
 
             int inputNumber = Int32.Parse(Console.ReadLine());
             switch (inputNumber)
@@ -61,20 +74,84 @@ internal class Program
                     Console.WriteLine("메인스레드 종료 ");
                     break;
                 case 2:
-                    Thread thread12 = new Thread(ThreadPolling);
-                    thread12.Start();
+                    int sum = 0; 
+                    var thread2 = new Thread(() =>
+                    {
+                        for (int i = 0; i < 10; i++)
+                        {
+                            Console.WriteLine(DateTime.Now.ToString("yyyy/MM/dd hh:mm:ssss"));
+                            sum++; 
+                        }
+                    });
+                    thread2.Start();
+                    Console.WriteLine("Thread 2");
+                    thread2.Join(); //Join이 있어야 Thread를 Waiting... 
+                    Console.WriteLine(sum);
+                    break;
+                case 3:
+                    Thread thread3 = new Thread(ThreadPolling);
+                    thread3.Start();
                     for (int i = 0; i < 3; i++)
                     {
                         Console.WriteLine(i + 1);
                         Thread.Sleep(100);
                     }
                     myFlag = true;
-                    thread12.Abort();
-                    thread12.Join();
+                    thread3.Abort();
+                    thread3.Join();
                     Console.WriteLine("메인쓰레드 종료 ");
-                    
                     break;
+                case 4:
+                    Thread thread4 = new Thread(ThreadPolling);
+                    thread4.Start();
+                    for (int i = 0; i < 3; i++)
+                    {
+                        Console.WriteLine(i + 1);
+                        Thread.Sleep(100);
+                    }
+                    myFlag = true;
+                    //thread3.Abort();
+                    //thread3.Join();
+                    thread4.Interrupt();
+                    Console.WriteLine("메인쓰레드 종료 ");
+                    break;
+                case 5:
+                    Counter counter = new CounterClass.Counter();
 
+                    Thread t1 = new Thread(new ThreadStart(counter.Increase));
+                    Thread t2 = new Thread(new ThreadStart(counter.Decrease));
+
+                    t1.Start();
+                    t2.Start();
+                    t1.Join();
+                    t2.Join();
+
+                    Console.WriteLine(counter.Count);
+
+                    break;
+                case 6:
+                    MutexCounter mutexCounter = new MutexCounter();
+                    Thread first =  new Thread(new ThreadStart(mutexCounter.FirstWork));
+                    Thread second = new Thread(new ThreadStart(mutexCounter.SecondWork));
+                    first.Start(); 
+                    second.Start();
+
+                    first.Join();
+                    second.Join();
+                    break;
+                case 11:
+                    PrintThreadState(ThreadState.Running);
+                    PrintThreadState(ThreadState.StopRequested);
+                    PrintThreadState(ThreadState.SuspendRequested);
+                    PrintThreadState(ThreadState.Background);
+                    PrintThreadState(ThreadState.Unstarted);
+                    PrintThreadState(ThreadState.Stopped);
+                    PrintThreadState(ThreadState.WaitSleepJoin);
+                    PrintThreadState(ThreadState.Suspended);
+                    PrintThreadState(ThreadState.AbortRequested);
+                    PrintThreadState(ThreadState.Aborted);
+                    PrintThreadState(ThreadState.Aborted | ThreadState.Stopped);
+                    break;
                 default:
                     Console.WriteLine("다시 재 선택 하시오. ");
                     break;
